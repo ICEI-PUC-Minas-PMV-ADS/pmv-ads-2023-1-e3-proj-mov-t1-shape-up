8ddf1640d21749a403921458f769cfbf347f550e
RaulShinaede <rshinaede@outlook.com>
2023-04-28 11:31:05 -0300

    Adicionando arquivos de páginas

--- app/shape-up/App.js
+++ app/shape-up/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { extendTheme, NativeBaseProvider } from "native-base";
+import { theme } from './assets/resources/Theme';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-        <View>
-            <Text>Shape Up</Text>
-        </View>
+        <NativeBaseProvider theme={theme}>
+
+        </NativeBaseProvider>
     </SafeAreaProvider>
   );
 }
---
+++ app/shape-up/assets/resources/Theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "native-base";
+
+export const theme = extendTheme({
+    colors: {
+        primary: {
+            900: "#8287af",
+            800: "#7c83db",
+            700: "#b3bef6",
+        }
+    }
+});
