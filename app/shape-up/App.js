b4aff1f9a98a16a97ab0750815be7d928fa22534
RaulShinaede <rshinaede@outlook.com>
2023-04-28 11:03:07 -0300

    Configurando expo app

---
+++ app/shape-up/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+        <View>
+            <Text>Shape Up</Text>
+        </View>
+    </SafeAreaProvider>
+  );
+}
