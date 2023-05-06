5299cb86ed5faf11e0c10d4b4eec4ac402bd55a6
Raul Oliveira <rshinaede@outlook.com>
2023-05-06 16:17:44 -0300

    Retrabalho dos componentes do cadastro

--- app/shape-up/src/pages/CadastrePage.js
+++ app/shape-up/src/pages/CadastrePage.js
@@ -8,24 +8,18 @@ import {
     TouchableWithoutFeedback,
     Keyboard,
     Image,
-    Text
   } from 'react-native';
-  import { Icon, Input, Button, Pressable } from 'native-base';
+  import { Button } from 'native-base';
   import Carousel, { Pagination } from 'react-native-snap-carousel';
-  import { Card } from '../components/Card';
-  import { MaterialCommunityIcons } from '@expo/vector-icons'; 
-  import { MaterialIcons } from "@expo/vector-icons";
-  import * as ImagePicker from 'expo-image-picker';
-
-  let carousel = {};
-  let nav = {};
+  import { InputEmailCard } from '../components/InputEmailCard';
+  import { InputNameCard } from '../components/InputNameCard';
+  import { InputImageCard } from '../components/InputImageCard';
+  import { InputPasswordCard } from '../components/InputPasswordCard';
+  import { register } from '../services/auth.services';
 
   export function CadastrePage({navigation}) {
 
-    carousel = React.useRef(null)
-    nav = navigation;
-
-    const [show, setShow] = React.useState(false);
+    const carousel = React.useRef(null)
     const {height, width} = useWindowDimensions();
     const [index, setIndex] = React.useState(0);
 
@@ -33,24 +27,43 @@ import {
 
     const [name, setName] = React.useState(null);
     const [email, setEmail] = React.useState(null);
+    const [imageData, setImageData] = React.useState(null);
     const [password, setPassword] = React.useState(null);
-    const [rePassword, setRePassword] = React.useState(null);
-    const [imageUri, setImageUri] = React.useState(null);
 
-    const handleSelectImage = () => {
+    const handleBack = function() {
+        if (index == 0) {
+            handleGoToLogin();
+        } else {
+            carousel.current._snapToItem(carousel.current._getPositionIndex(index - 1));
+        } 
+    }
 
-        ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images,
-          allowsEditing: true,
-          quality: 1,
-        }).then(result => {
-            if (!result.canceled) {
-                setImageUri(result.uri);
-              } else {
-                alert('You did not select any image.');
-              }
-        })
-      };
+    const handleNext = function() {
+        carousel.current._snapToItem(carousel.current._getPositionIndex(index + 1));
+    }
+
+    const handleGoToLogin = function() {
+        navigation.navigate('Login');
+    }
+
+    const renderCarouselItems = function({item, index}) {
+        switch(index) {
+            case 0:
+                return (<InputNameCard setValue={setName} handleNext={handleNext} handleGoToLogin={handleGoToLogin}></InputNameCard>);
+            case 1:
+                return (<InputEmailCard setValue={setEmail} handleNext={handleNext} handleGoToLogin={handleGoToLogin}></InputEmailCard>);
+            case 2:
+                return (<InputImageCard setValue={setImageData} handleNext={handleNext} handleGoToLogin={handleGoToLogin}></InputImageCard>);
+            case 3:
+                return (<InputPasswordCard setValue={setPassword} handleNext={handleCreateAccount} handleGoToLogin={handleGoToLogin}></InputPasswordCard>);
+        }
+    }
+
+    const handleCreateAccount = function() {
+        if(register(name, email, password, imageData)) {
+            navigation.navigate('Welcome');
+        }
+    }
 
     return (
     <View style={styles.inner}>
@@ -74,115 +87,20 @@ import {
                                 useScrollView={false}
                                 scrollEnabled={false}
                                 onSnapToItem={(index) => setIndex(index)}
-                                renderItem={({ item, index }) => {
-                                    switch(index) {
-                                        case 0:
-                                            return (
-                                                <Card>
-                                                    <MaterialCommunityIcons name="account" size={48} color="#ff4444" marginTop={40}/>
-                                                    <Text marginTop={20} style={styles.cardTitle}>Qual é o seu nome?</Text>
-                                                    <Input w={'75%'} marginTop={10} placeholder='Nome' color='#ffffff' />
-                                                    <Button w='60%' variant='outline' marginTop={6} borderColor='#ff4444'  _text={{fontWeight: 'bold', fontSize: 16, fontFamily: 'Roboto'}} 
-                                                        onPress={() => carousel.current._snapToItem(carousel.current._getPositionIndex(index + 1))}>Confirmar</Button>
-                                                    <TouchableWithoutFeedback onPress={() => nav.navigate('Login')}>
-                                                        <View style={{flex: 1, flexDirection: 'row'}}>
-                                                            <Text style={styles.cardHasAccountText}>Já tem uma conta?</Text>
-                                                            <Text style={styles.cardLoginText}>Login</Text>
-                                                        </View>
-                                                    </TouchableWithoutFeedback>
-                                                </Card>
-                                            );
-                                        case 1:
-                                            return (
-                                                <Card>
-                                                    <MaterialCommunityIcons name="at" size={48} color="#ff4444" marginTop={40}/>
-                                                    <Text marginTop={20} style={styles.cardTitle}>Qual é o seu email?</Text>
-                                                    <Input w={'75%'} marginTop={10} placeholder='Email' color='#ffffff'/>
-                                                    <Button w='60%' variant='outline' marginTop={6} borderColor='#ff4444'  _text={{fontWeight: 'bold', fontSize: 16, fontFamily: 'Roboto'}}
-                                                        onPress={() => carousel.current._snapToItem(carousel.current._getPositionIndex(index + 1))}>Confirmar</Button>
-                                                    <TouchableWithoutFeedback onPress={() => nav.navigate('Login')}>
-                                                        <View style={{flex: 1, flexDirection: 'row'}}>
-                                                            <Text style={styles.cardHasAccountText}>Já tem uma conta?</Text>
-                                                            <Text style={styles.cardLoginText}>Login</Text>
-                                                        </View>
-                                                    </TouchableWithoutFeedback>
-                                                </Card>
-                                            );
-                                        case 2:
-                                            return (
-                                                <Card>
-                                                    <MaterialCommunityIcons name="image-outline" size={48} color="#ff4444" marginTop={40}/>
-                                                    <Text marginTop={20} style={styles.cardTitle}>Carregar foto de perfil</Text>
-                                                    <Button w='80%' mt={6} backgroundColor='#ff4444' _text={{color: '#2e2e2e', fontSize: 16, fontWeight: 'bold'}} onPress={() => handleSelectImage()}>Carregar</Button>
-                                                    <Text style={{
-                                                        width: '80%',
-                                                        fontSize: 12,
-                                                        fontWeight: '600',
-                                                        color: '#c2c2c2',
-                                                        marginLeft: 20,
-                                                        marginTop: 10
-                                                    }}>Nenhuma foto carregada</Text>
-                                                    <Button w='60%' variant='outline' marginTop={4} borderColor='#ff4444'  _text={{fontWeight: 'bold', fontSize: 16, fontFamily: 'Roboto'}}
-                                                        onPress={() => carousel.current._snapToItem(carousel.current._getPositionIndex(index + 1))}>Confirmar</Button>
-                                                    <TouchableWithoutFeedback onPress={() => nav.navigate('Login')}>
-                                                        <View style={{flex: 1, flexDirection: 'row'}}>
-                                                            <Text style={styles.cardHasAccountText}>Já tem uma conta?</Text>
-                                                            <Text style={styles.cardLoginText}>Login</Text>
-                                                        </View>
-                                                    </TouchableWithoutFeedback>
-                                                </Card>
-                                            );
-                                        case 3:
-                                            return (
-                                                <Card>
-                                                    <MaterialCommunityIcons name="lock" size={48} color="#ff4444" marginTop={20}/>
-                                                    <Text marginTop={10} style={styles.cardTitle}>Digite sua senha</Text>
-                                                    <View style={{ width: '80%', height: 100, justifyContent: "space-between", marginTop: 10, marginBottom: 10}}>
-                                                        <Input color='light.50' type={show ? "text" : "password"} InputRightElement={
-                                                            <Pressable onPress={() => setShow(!show)}>
-                                                                <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" />
-                                                            </Pressable>}  placeholder="Password"/>
-                                                        <Input color='light.50' type={show ? "text" : "password"} InputRightElement={
-                                                            <Pressable onPress={() => setShow(!show)}>
-                                                                <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" />
-                                                            </Pressable>}  placeholder="Password"/>
-                                                        
-                                                    </View>
-                                                    <Button w='60%' variant='outline' marginTop={4} borderColor='#ff4444'  _text={{fontWeight: 'bold', fontSize: 16, fontFamily: 'Roboto'}}
-                                                        onPress={() => navigation.navigate('Welcome')}>Criar conta</Button>
-                                                    <TouchableWithoutFeedback onPress={() => nav.navigate('Login')}>
-                                                        <View style={{flex: 1, flexDirection: 'row'}}>
-                                                            <Text style={styles.cardHasAccountText}>Já tem uma conta?</Text>
-                                                            <Text style={styles.cardLoginText}>Login</Text>
-                                                        </View>
-                                                    </TouchableWithoutFeedback>
-                                                </Card>
-                                            );
-                                        }
-                                }}/>
+                                renderItem={renderCarouselItems}/>
                         </View>
 
                         <View style={styles.bottomNavContainer}>
-                            <Button style={styles.bottomNavItem} _text={styles.bottomNavItemText} 
-                                onPress={() => carousel.current._snapToItem(carousel.current._getPositionIndex(index - 1))}>Voltar</Button>
-                            <Button style={styles.bottomNavItem} _text={styles.bottomNavItemText} 
-                                onPress={() => carousel.current._snapToItem(carousel.current._getPositionIndex(index + 1))}>Pular</Button>
+                            <Button style={styles.bottomNavItem} _text={styles.bottomNavItemText} onPress={handleBack}>Voltar</Button>
+                            <Button isDisabled={index == 2 ? false : true} style={styles.bottomNavItem} _text={styles.bottomNavItemText} onPress={handleNext}>Pular</Button>
                         </View>
-
 
                         <Pagination
                             style={{borderColor: 'black', borderWidth: 2}}
                             dotsLength={4}
                             activeDotIndex={index}
                             carouselRef={carousel}
-                            dotStyle={{
-                                width: 10,
-                                height: 10,
-                                borderRadius: 5,
-                                borderColor: '#ff4444',
-                                marginHorizontal: 0,
-                                backgroundColor: '#ff4444'
-                            }}
+                            dotStyle={styles.dotStyle}
                             tappableDots={true}
                             inactiveDotOpacity={0.4}
                             inactiveDotScale={0.6}/>
@@ -202,38 +120,19 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center'
     },
+    dotStyle: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        borderColor: '#ff4444',
+        marginHorizontal: 0,
+        backgroundColor: '#ff4444'
+    },
     logo: {
         width: 300,
         height: 150,
         alignSelf: 'center',
         marginTop: 50
-    },
-    dividerText: {
-        color: '#dedede',
-        marginRight: 10,
-        marginLeft: 10,
-        fontSize: 18
-    },
-    cardTitle: {
-        fontFamily: 'Roboto',
-        fontWeight: 'bold',
-        fontSize: 20,
-        color: '#ffffff',
-    },
-    cardHasAccountText: {
-        fontFamily: 'Roboto',
-        fontWeight: 'bold',
-        fontSize: 14,
-        color: '#C0C0C0',
-        marginTop: 18
-    },
-    cardLoginText: {
-        fontFamily: 'Roboto',
-        fontWeight: 'bold',
-        fontSize: 14,
-        color: '#1C7793',
-        marginTop: 18,
-        marginLeft: 3
     },
     bottomNavContainer: {
         flex: 0,
--- app/shape-up/src/services/auth.services.js
+++ app/shape-up/src/services/auth.services.js
@@ -2,13 +2,17 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import CryptoJS from 'crypto-js';
 
-export const register = async (name, username, password) => {
+export const register = async (name, username, password, imageData) => {
 
   var passHashed = CryptoJS.SHA1(password);
+  const formData = new FormData();
+  formData.append('imagem', imagemInput.files[0]);
 
   await axios.post('https://shape-up-backend-api.onrender.com/v1/auth/login', {
+    'name': name.toString(),
     'username': username.toString(),
-    'password': passHashed.toString()
+    'password': passHashed.toString(),
+    'imageData': imageData.toString()
   })
   .then(function (response) {
 
@@ -17,8 +21,9 @@ export const register = async (name, username, password) => {
     if (json.isAuthenticated) {
         AsyncStorage.setItem('ShapeUp:Token', json.token);
         AsyncStorage.setItem('ShapeUp:RefreshToken', json.refreshToken);
-        AsyncStorage.setItem('ShapeUp:LastUserName', json.name);
-        AsyncStorage.setItem('ShapeUp:LastUserEmail', json.username);
+        AsyncStorage.setItem('ShapeUp:UserName', json.name);
+        AsyncStorage.setItem('ShapeUp:UserEmail', json.username);
+        AsyncStorage.setItem('ShapeUp:UserImage', json.imageData);
 
         return true;
     } else {
@@ -53,8 +58,9 @@ export const login = async (username, password) => {
         if (json.isAuthenticated) {
             AsyncStorage.setItem('ShapeUp:Token', json.token);
             AsyncStorage.setItem('ShapeUp:RefreshToken', json.refreshToken);
-            AsyncStorage.setItem('ShapeUp:LastUserName', json.name);
-            AsyncStorage.setItem('ShapeUp:LastUserEmail', json.username);
+            AsyncStorage.setItem('ShapeUp:UserName', json.name);
+            AsyncStorage.setItem('ShapeUp:UserEmail', json.username);
+            AsyncStorage.setItem('ShapeUp:UserImage', json.imageData);
 
             return true;
         } else {
@@ -77,14 +83,15 @@ export const login = async (username, password) => {
 export const logout = async () => {
     AsyncStorage.removeItem('ShapeUp:Token');
     AsyncStorage.removeItem('ShapeUp:RefreshToken');
-    AsyncStorage.removeItem('ShapeUp:LastUserName');
-    AsyncStorage.removeItem('ShapeUp:LastUserEmail');
+    AsyncStorage.removeItem('ShapeUp:UserName');
+    AsyncStorage.removeItem('ShapeUp:UserEmail');
+    AsyncStorage.removeItem('ShapeUp:UserImage');
 };
 
 export const isAuthenticated = async() => {
 
     let token = AsyncStorage.getItem('ShapeUp:Token');
-    let username =  AsyncStorage.getItem('ShapeUp:LastUserEmail');
+    let username =  AsyncStorage.getItem('ShapeUp:UserEmail');
 
     if (!token || !username) {
         return false;
