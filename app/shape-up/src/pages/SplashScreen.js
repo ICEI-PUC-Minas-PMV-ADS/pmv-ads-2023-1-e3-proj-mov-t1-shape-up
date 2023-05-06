85ee5bfc78553086ae8010643a12d5f7a25dd5ff
Raul Oliveira <rshinaede@outlook.com>
2023-05-06 18:41:56 -0300

    Criando página de carregamento

---
+++ app/shape-up/src/pages/SplashScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Spinner } from 'native-base';
+import { isAuthenticated  } from "../services/auth.services";
+
+export function SplashScreen({navigation}) {
+
+    isAuthenticated()
+        .then((response) => {
+            if (response) {
+                navigation.navigate('Home');
+            } else {
+                navigation.navigate('Login');
+            }
+        });
+
+    return (
+        <View style={{height: '100%', width: '100%'}}>
+            <LinearGradient
+                            style={{height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center'}}
+	                        colors={['#ff4444','#000000']}
+                            start={{x:-1,y:-0.9}}
+                            end={{x:0.1,y:1}}>
+                       <Image style={{
+                           width: 300,
+                           height: 150,
+                           alignSelf: 'center',
+                           marginBottom: 100
+                       }} resizeMode='contain' source={require('../../assets/imgs/logomarca.png')}></Image>     
+                       <Spinner size={80}/>
+                </LinearGradient>
+        </View>
+    );
+}
