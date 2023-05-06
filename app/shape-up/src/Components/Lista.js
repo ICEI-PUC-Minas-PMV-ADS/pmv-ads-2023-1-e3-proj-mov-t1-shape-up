d0372e189b1a900f33c408c349277175519bb540
brunosellas <bruno_sellas@hotmail.com>
2023-05-05 22:18:37 -0300

    Termino da elaboração dos componentes de lista de treinos e preparação para receber dados do banco

--- app/shape-up/src/Components/Lista.js
+++ app/shape-up/src/Components/Lista.js
@@ -1,90 +1,95 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome'
-import moment from "moment";
-import 'moment/locale/pt-br'
+import { Checkbox } from 'react-native-paper';
+import { Image } from "native-base";
+
+
+
 
 
 export default props => {
-
+    const [checked, setChecked] = React.useState(false);
+    const imagePath = props.imagePath
     return (
         <View style={styles.container}>
             <View style={styles.checkContainer}>
-                {getCheckView(props.doneAt)}
-
+                <Checkbox
+                    status={checked ? 'checked' : 'unchecked'}
+                    onPress={() => {
+                        setChecked(!checked);
+                    }}
+                />
             </View>
-            <View>
+            <View style={styles.textView}>
                 <Text style={styles.desc}>{props.desc}</Text>
-                <Text style={styles.date}>{props.doneAt + ""}</Text>
+                <Text style={styles.numex}>{props.numEx}</Text>
             </View>
-
+            <View style={styles.imageView}>
+                <Image style={styles.image} source={{ uri: props.imagePath }} alt="Alternate Text" size="xl" />
+            </View>
         </View>
     )
 }
 
-function getCheckView(doneAt) {
-    if (doneAt != null) {
-        return (
-            <View style={styles.done}>
-                <Icon name='check' size={20} color={'#FFF'}></Icon>
-            </View>
-        )
-    } else {
-        return (
-            <View style={styles.pending}></View>
-        )
-    }
-
-}
 
 const styles = StyleSheet.create({
     container: {
-        width: '80%',
+        height: 100,
+        width: '90%',
         flexDirection: 'row',
         borderRadius: 15,
         borderColor: '#FFF',//Alterar
         borderWidth: 1,
         alignItems: 'center',
-        paddingVertical: 10,
         marginBottom: 28
-
 
     },
     checkContainer: {
         width: '20%',
+        height: '100%',
         alignItems: 'center',
-        justifyContent: 'center'
-    },
-
-
-    pending: {
-        height: 25,
-        width: 25,
-        borderRadius: 13,
-        borderWidth: 1,
-        borderColor: '#FFF'
+        justifyContent: 'center',
 
     },
 
-    done: {
-        height: 25,
-        width: 25,
-        borderRadius: 13,
-        backgroundColor: 'green',
-        alignItems: 'center',
-        justifyContent: 'center'
-
-
-    },
 
     desc: {
         fontFamily: 'Roboto',
-        color: '#FFF'
+        color: '#FFF',
+        marginRight: 20
 
     },
 
-    date: {
-        color: '#C6C9CE',
-    }
+    numex: {
+        fontFamily: 'Roboto',
+        color: 'grey'
+    },
 
+    textView: {
+        width: '40%',
+
+
+
+    },
+
+
+    imageView: {
+        height: '100%',
+        width: '40%',
+        right: 0,
+
+
+    },
+
+    image: {
+        resizeMode: 'cover',
+        width: '100%',
+        height: '100%',
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 50,
+
+
+    },
 })
--- app/shape-up/src/pages/HomePage.js
+++ app/shape-up/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, Image } from "react-native";
 import Header from "../Components/Header"
 import Lista from "../Components/Lista"
 
@@ -22,8 +22,8 @@ export function HomePage() {
                 <Text style={styles.contentTxt}>{Saudacao()} {name}</Text>
                 <Text style={styles.contentTxt}>Por onde vamos começar?</Text>
                 <View style={styles.containerLista}>
-                    <Lista desc="Treino de Peito" doneAt={new Date()}></Lista>
-                    <Lista desc="Treino de Braços" doneAt={null}></Lista>
+                    <Lista desc="Treino de Peito" numEx={"5 exercícios"} imagePath='https://img.freepik.com/fotos-gratis/homem-musculoso-fazendo-flexoes-usando-halteres_7502-4776.jpg?w=1380&t=st=1683332069~exp=1683332669~hmac=505602e525d8927c23854ef6bc1752d968c9f4c0cb28cfc1fa996b06e40fff44'></Lista>
+                    <Lista desc="Treino de Braços" numEx={"6 exercícios"} imagePath='https://img.freepik.com/fotos-gratis/homem-forte-treinando-na-academia_1303-23478.jpg?w=1380&t=st=1683333513~exp=1683334113~hmac=3c6c4e5d32331cae27b5aaf51cc2f8b2dd89780632f361c4f40022ee28f4d7d0'></Lista>
                 </View>
             </View>
         </View>
@@ -51,5 +51,6 @@ const styles = StyleSheet.create({
         fontSize: 18,
 
     },
+
 })
 
