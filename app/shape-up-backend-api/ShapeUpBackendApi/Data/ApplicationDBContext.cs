362eb28afb311db69f6dac5660d04699c1055390
RaulShinaede <rshinaede@outlook.com>
2023-05-31 17:04:34 -0300

    Criando tabelas de treino na api

--- app/shape-up-backend-api/ShapeUpBackendApi/Data/ApplicationDBContext.cs
+++ app/shape-up-backend-api/ShapeUpBackendApi/Data/ApplicationDBContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ShapeUpBackendApi.Authentication.Models;
+using ShapeUpBackendApi.Training.Models;
 
 namespace ShapeUpBackendApi.Data {
     public class ApplicationDBContext : DbContext {
@@ -9,5 +10,22 @@ namespace ShapeUpBackendApi.Data {
 
         public DbSet<User> Users { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<ExerciseDetail> ExerciseDetails { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            modelBuilder.Entity<Workout>()
+                .HasMany(w => w.Exercises)
+                .WithOne()
+                .HasForeignKey(w => w.WorkoutId);
+
+            modelBuilder.Entity<ExerciseDetail>()
+                .HasOne(ed => ed.Exercise)
+                .WithMany()
+                .HasForeignKey(ed => ed.ExerciseId);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
